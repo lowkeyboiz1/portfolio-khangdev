01d1a02bdabed587cdeef5d017107edd0dfb26c4
lowkeyboiz1 <khangluong20021@gmail.com>
2025-01-08 10:27:27 +0700

    my skills

--- src/components/MySkills/MySkills.tsx
+++ src/components/MySkills/MySkills.tsx
@@ -1,12 +1,138 @@
 import React from 'react'
+import JavascriptIcon from '@/assets/langsAndTools/javascript.svg'
+import TypeScriptIcon from '@/assets/langsAndTools/typescript.svg'
+import FigmaIcon from '@/assets/langsAndTools/figma.svg'
+import HtmlIcon from '@/assets/langsAndTools/html.svg'
+import NodeIcon from '@/assets/langsAndTools/nodejs.svg'
+import SassIcon from '@/assets/langsAndTools/sass.svg'
+import CssIcon from '@/assets/langsAndTools/css.svg'
+import TanstackIcon from '@/assets/langsAndTools/tanstack-query.svg'
+
+import ReactIcon from '@/assets/libbrariesAndFrameworks/react.svg'
+import ReduxIcon from '@/assets/libbrariesAndFrameworks/redux.svg'
+import NextIcon from '@/assets/libbrariesAndFrameworks/nextjs.svg'
+import NextUiIcon from '@/assets/libbrariesAndFrameworks/nextui.png'
+import TailwindIcon from '@/assets/libbrariesAndFrameworks/tailwindcss.svg'
+import ShadcnIcon from '@/assets/libbrariesAndFrameworks/shadcn.png'
+import ZustandIcon from '@/assets/libbrariesAndFrameworks/zustand.svg'
+
+import MongoIcon from '@/assets/db/mongodb.svg'
+import { AnimatedTooltip } from '../AnimatedTooltip'
 
 const MySkills = () => {
+  const langsAndTools = [
+    {
+      id: 1,
+      name: 'HTML',
+      image: HtmlIcon
+    },
+    {
+      id: 2,
+      name: 'CSS',
+      image: CssIcon
+    },
+    {
+      id: 3,
+      name: 'JavaScript',
+      image: JavascriptIcon
+    },
+    {
+      id: 4,
+      name: 'TypeScript',
+      image: TypeScriptIcon
+    },
+    {
+      id: 5,
+      name: 'Sass',
+      image: SassIcon
+    },
+    {
+      id: 6,
+      name: 'Node.js',
+      image: NodeIcon
+    },
+    {
+      id: 7,
+      name: 'Figma',
+      image: FigmaIcon
+    },
+    {
+      id: 8,
+      name: 'Tanstack Query',
+      image: TanstackIcon
+    }
+  ]
+
+  const librariesAndFrameworks = [
+    {
+      id: 9,
+      name: 'React',
+      image: ReactIcon
+    },
+    {
+      id: 10,
+      name: 'Next.js',
+      image: NextIcon
+    },
+    {
+      id: 12,
+      name: 'Tailwind',
+      image: TailwindIcon
+    },
+    {
+      id: 11,
+      name: 'Next UI',
+      image: NextUiIcon
+    },
+
+    {
+      id: 13,
+      name: 'Shadcn',
+      image: ShadcnIcon
+    },
+    {
+      id: 14,
+      name: 'Redux',
+      image: ReduxIcon
+    },
+    {
+      id: 15,
+      name: 'Zustand',
+      image: ZustandIcon
+    }
+  ]
+  const db = [
+    {
+      id: 16,
+      name: 'MongoDB',
+      image: MongoIcon
+    }
+  ]
   return (
-    <div className='page'>
-      <div className='gradient-text text-6xl font-bold'>My Skills</div>
-      <div className='mt-2 max-w-sm text-2xl font-medium leading-[2] md:max-w-lg'>I like to take responsibility to craft aesthetic user experience using modern frontend architecture.</div>
-      <div className='space-y-10'>
-        <p className='text-lg font-medium tracking-widest'>LANGUAGES AND TOOLS</p>
+    <div className='flex flex-col gap-14 py-10 page'>
+      <div className='space-y-2'>
+        <div className='gradient-text text-6xl font-bold'>My Skills</div>
+        <div className='mt-2 max-w-sm text-2xl font-medium leading-[2] md:max-w-3xl'>I like to take responsibility to craft aesthetic user experience using modern frontend architecture.</div>
+      </div>
+      <div className='space-y-6'>
+        <SkillCard title='LANGUAGES AND TOOLS' items={langsAndTools} />
+        <SkillCard title='LIBRARIES AND FRAMEWORKS' items={librariesAndFrameworks} />
+        <SkillCard title='DATABASES' items={db} />
+      </div>
+    </div>
+  )
+}
+
+const SkillCard = ({ title, items }: { title: string; items: { id: number; name: string; image: string }[] }) => {
+  return (
+    <div className='space-y-6'>
+      <p className='text-lg font-medium tracking-widest'>{title}</p>
+      <div className='flex gap-10'>
+        {items.map((item) => (
+          <div key={item.name} className='h-14'>
+            <AnimatedTooltip item={item} />
+          </div>
+        ))}
       </div>
     </div>
   )
